514231dd4905dff35a6c602fc00b78c42d228045
dongqilin <505235893@qq.com>
2019-07-03 11:14:06 +0800

    tk.mybatis

---
+++ src/main/java/com/smart/mapper/BaseMapper.java
@@ -0,0 +1,11 @@
+package com.smart.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @description:
+ * @author: dongql
+ * @date: 2019/6/13 17:37
+ */
+public interface BaseMapper<T> extends Mapper<T> {
+}
